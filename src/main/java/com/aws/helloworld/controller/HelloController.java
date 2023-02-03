7483c099a8601cbddea792a8e060b373b2f2bae0
Sravan Kumar Katta <sr.katta@l3rcjcm725.replynet.prv>
2023-02-03 10:51:00 +0100

    changing package to install build

--- src/main/java/com/aws/helloworld/controller/HelloController.java
+++ src/main/java/com/aws/helloworld/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.aws.helloworld.controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,7 +7,7 @@ public class HelloController {
 
     @GetMapping
     public String test() {
-        return "home page";
+        return "home";
     }
 
     @GetMapping("/hello")
